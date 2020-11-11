02a89942de52872a024a6053d9175cf090b236cf
Henok Mekonnen <henatan99@gmail.com>
2020-11-11 18:10:25 +0200

    Adds the following methods to the 'Enumerable' module
    - Adds my_each_with_index, my_select, my_all, my_any, my_none, my_count
      methods to the module
    - All the methods except 'my_each_with_index' method calls the 'my_each'
      method to iterate through the given array

--- main.rb
+++ main.rb
@@ -1,15 +1,61 @@
 module Enumerable
-    def my_each                
-        n = self.length      
-        (n).times do |index|               
-        self[index] = yield(self[index])       
-        end 
+    def my_each
+        n = self.length
+        (n-1).times do |i|
+        self[i] = yield(self[i])
+        end
         self
     end
+    def my_each_with_index
+        n = self.length
+        (n).times do |index|
+        self[index] = yield(self[index])
+        end
+        self
+    end
+    def my_select
+        arr = []
+        self.my_each do |elem|
+            if yield(elem) == true
+                arr << elem
+            end
+        end
+        arr
+    end
+    def my_all
+        self.my_each do |elem|
+            if yield(elem) == false
+                return false
+            end
+        end
+        return true
+    end
+    def my_any
+        self.my_each do |elem|
+            if yield(elem) == true
+                return true
+            end
+        end
+        return false
+    end
+    def my_none
+        self.my_each do |elem|
+            if yield(elem) == true
+                return false
+            end
+        end
+        return true
+    end
+    def my_count
+        count = 0
+        self.my_each do |elem|
+            if yield(elem) == true
+                count += 1
+            end
+        end
+        count
+    end
 end
-
-a = [1,2,3,4] 
-
+a = [1,2,3,4]
 a.my_each {|elem| elem+1}
-
 
